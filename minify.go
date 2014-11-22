b15e5042d8500ff353c75e1d4d1fe28e9e1b67bd
Taco de Wolff <tacodewolff@gmail.com>
2014-11-22 15:35:29 +0100

    Refactored JS function away
    user can implement minifiers
    convenience function to add a command line program to process the
    minification

--- minify.go
+++ minify.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"io"
 	"io/ioutil"
+	"os/exec"
 
 	"github.com/kballard/go-shellquote"
 )
@@ -13,32 +14,70 @@ var ErrNotExist = errors.New("minifier does not exist for mime type")
 
 ////////////////////////////////////////////////////////////////
 
-type Minifier func(Minify, io.Reader) (io.Reader, error)
+type MinifyFunc func(Minify, io.Reader) (io.Reader, error)
 
 type Minify struct {
-	MimeMinifier map[string]Minifier
-	JsMinifier   []string
+	Minifier map[string]MinifyFunc
 }
 
-func NewMinify(jsMinifier string) *Minify {
-	jsMinifierCmd, err := shellquote.Split(jsMinifier)
-	if err != nil {
-		jsMinifierCmd = []string{}
-	}
-
+func NewMinify() *Minify {
 	return &Minify{
-		map[string]Minifier{
+		map[string]MinifyFunc{
 			"text/html":              (Minify).Html,
-			"text/javascript":        (Minify).Js,
-			"application/javascript": (Minify).Js,
 			"text/css":               (Minify).Css,
 		},
-		jsMinifierCmd,
 	}
+}
+
+func (m *Minify) Implement(mime string, f MinifyFunc) {
+	m.Minifier[mime] = f
+}
+
+func (m *Minify) ImplementCmd(mime string, cmdString string) error {
+	cmdSplit, err := shellquote.Split(cmdString)
+	if err != nil {
+		return err
+	}
+
+	m.Minifier[mime] = func (m Minify, r io.Reader) (io.Reader, error) {
+		var cmd *exec.Cmd
+		if len(cmdSplit) == 1 {
+			cmd = exec.Command(cmdSplit[0])
+		} else {
+			cmd = exec.Command(cmdSplit[0], cmdSplit[1:]...)
+		}
+
+		stdOut, err := cmd.StdoutPipe()
+		if err != nil {
+			return r, err
+		}
+
+		stdIn, err := cmd.StdinPipe()
+		if err != nil {
+			return r, err
+		}
+
+		if err = cmd.Start(); err != nil {
+			return r, err
+		}
+		if _, err := io.Copy(stdIn, r); err != nil {
+			return nil, err
+		}
+		stdIn.Close()
+
+		b := new(bytes.Buffer)
+		if _, err = io.Copy(b, stdOut); err != nil {
+			return nil, err
+		}
+		stdOut.Close()
+
+		return b, cmd.Wait()
+	}
+	return nil
 }
 
 func (m Minify) Filter(mime string, r io.Reader) (io.Reader, error) {
-	if f, ok := m.MimeMinifier[mime]; ok {
+	if f, ok := m.Minifier[mime]; ok {
 		r, err := f(m, r)
 		if err != nil {
 			return nil, err
