421a6a9bd19fe19faa690cd1433eb693c1ea4b9e
Taco de Wolff <tacodewolff@gmail.com>
2015-04-25 18:47:59 +0200

    JSON added

---
+++ json/json.go
@@ -0,0 +1,57 @@
+package json // import "github.com/tdewolff/minify/json"
+
+import (
+	"io"
+
+	"github.com/tdewolff/minify"
+	"github.com/tdewolff/parse/json"
+)
+
+var (
+	quoteBytes = []byte("\"")
+	commaBytes = []byte(",")
+	colonBytes = []byte(":")
+)
+
+// Minify minifies JSON files, it reads from r and writes to w.
+func Minify(_ minify.Minifier, _ string, w io.Writer, r io.Reader) error {
+	skipComma := true
+	z := json.NewTokenizer(r)
+	for {
+		state := z.State()
+		tt, text := z.Next()
+		if tt == json.ErrorToken {
+			if z.Err() != io.EOF {
+				return z.Err()
+			}
+			return nil
+		}
+
+		if !skipComma && tt != json.EndObjectToken && tt != json.EndArrayToken {
+			if state == json.ObjectKeyState || state == json.ArrayState {
+				if _, err := w.Write(commaBytes); err != nil {
+					return err
+				}
+			} else if state == json.ObjectValueState {
+				if _, err := w.Write(colonBytes); err != nil {
+					return err
+				}
+			}
+		}
+		skipComma = tt == json.StartObjectToken || tt == json.StartArrayToken
+
+		if tt == json.StringToken {
+			if _, err := w.Write(quoteBytes); err != nil {
+				return err
+			}
+			if _, err := w.Write(text); err != nil {
+				return err
+			}
+			if _, err := w.Write(quoteBytes); err != nil {
+				return err
+			}
+		} else if _, err := w.Write(text); err != nil {
+			return err
+		}
+	}
+}
---
+++ json/json_test.go
@@ -0,0 +1,21 @@
+package json // import "github.com/tdewolff/minify/json"
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/tdewolff/minify"
+)
+
+func assertJSON(t *testing.T, input, expected string) {
+	m := minify.New()
+	b := &bytes.Buffer{}
+	assert.Nil(t, Minify(m, "application/json", b, bytes.NewBufferString(input)), "Minify must not return error in "+input)
+	assert.Equal(t, expected, b.String(), "Minify must give expected result in "+input)
+}
+
+func TestCSS(t *testing.T) {
+	assertJSON(t, "{ \"a\": [1, 2] }", "{\"a\":[1,2]}")
+	assertJSON(t, "[{ \"a\": [{\"x\": null}, true] }]", "[{\"a\":[{\"x\":null},true]}]")
+}
