f70053df2d87ac2be15057e7b192f0e0e134795b
Taco de Wolff <tacodewolff@gmail.com>
2015-07-25 12:47:38 +0200

    Reformat tests with backticks

--- svg/svg_test.go
+++ svg/svg_test.go
@@ -22,23 +22,23 @@ func assertSVG(t *testing.T, input, expected string) {
 ////////////////////////////////////////////////////////////////
 
 func TestSVG(t *testing.T) {
-	assertSVG(t, "<!-- comment -->", "")
-	assertSVG(t, "<!DOCTYPE foo SYSTEM \"Foo.dtd\">", "")
-	assertSVG(t, "<?xml version=\"1.0\" ?>", "")
-	assertSVG(t, "<style> <![CDATA[ x ]]> </style>", "<style>x</style>")
-	assertSVG(t, "<svg version=\"1.0\"></svg>", "<svg/>")
-	assertSVG(t, "<svg x=\" a \"/>", "<svg x=\"a\"/>")
+	assertSVG(t, `<!-- comment -->`, ``)
+	assertSVG(t, `<!DOCTYPE foo SYSTEM "Foo.dtd">`, ``)
+	assertSVG(t, `<?xml version="1.0" ?>`, ``)
+	assertSVG(t, `<style> <![CDATA[ x ]]> </style>`, `<style>x</style>`)
+	assertSVG(t, `<svg version="1.0"></svg>`, `<svg/>`)
+	assertSVG(t, `<svg x=" a "/>`, `<svg x="a"/>`)
 	assertSVG(t, "<svg x=\" a \n b \"/>", "<svg x=\"a b\"/>")
-	assertSVG(t, "<svg x=\"5.0px\"/>", "<svg x=\"5px\"/>")
-	assertSVG(t, "<path d=\"M 100 100 L 300 100 L 200 100 z\"/>", "<path d=\"M100 100L300 100 200 100z\"/>")
-	assertSVG(t, "<path d=\"M100 -100M200 300z\"/>", "<path d=\"M100-100 200 300z\"/>")
-	assertSVG(t, "<path d=\"M0.5 0.6 M -100 0.5z\"/>", "<path d=\"M.5.6-100 .5z\"/>")
-	assertSVG(t, "<path d=\"M01.0 0.6 z\"/>", "<path d=\"M1 .6z\"/>")
-	assertSVG(t, `<?xml version="1.0" encoding="utf-8"?>`, "")
+	assertSVG(t, `<svg x="5.0px" y="0%"/>`, `<svg x="5px" y="0"/>`)
+	assertSVG(t, `<path d="M 100 100 L 300 100 L 200 100 z"/>`, `<path d="M100 100L300 100 200 100z"/>`)
+	assertSVG(t, `<path d="M100 -100M200 300z"/>`, `<path d="M100-100 200 300z"/>`)
+	assertSVG(t, `<path d="M0.5 0.6 M -100 0.5z"/>`, `<path d="M.5.6-100 .5z"/>`)
+	assertSVG(t, `<path d="M01.0 0.6 z"/>`, `<path d="M1 .6z"/>`)
+	assertSVG(t, `<?xml version="1.0" encoding="utf-8"?>`, ``)
 	assertSVG(t, `<svg viewbox="0 0 16 16"/>`, `<svg viewbox="0 0 16 16"/>`)
 
 	// from SVGO
-	assertSVG(t, `<!DOCTYPE bla><?xml?><!-- comment --><metadata/>`, "")
+	assertSVG(t, `<!DOCTYPE bla><?xml?><!-- comment --><metadata/>`, ``)
 }
 
 ////////////////////////////////////////////////////////////////
