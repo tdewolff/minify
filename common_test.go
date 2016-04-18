a0e5e05badc96004da0f423533fd5953bcb83020
Taco de Wolff <tacodewolff@gmail.com>
2016-04-19 00:47:32 +0200

    Random testing

--- common_test.go
+++ common_test.go
@@ -1,9 +1,12 @@
 package minify // import "github.com/tdewolff/minify"
 
 import (
+	"fmt"
 	"io"
 	"io/ioutil"
+	"math"
 	"math/rand"
+	"strconv"
 	"testing"
 
 	"github.com/tdewolff/test"
@@ -56,45 +59,110 @@ func TestDataURI(t *testing.T) {
 func TestNumber(t *testing.T) {
 	numberTests := []struct {
 		number   string
+		expected string
+	}{
+		{"0", "0"},
+		{".0", "0"},
+		{"1.0", "1"},
+		{"0.1", ".1"},
+		{"+1", "1"},
+		{"-1", "-1"},
+		{"-0.1", "-.1"},
+		{"100", "100"},
+		{"1000", "1e3"},
+		{"0.001", ".001"},
+		{"0.0001", "1e-4"},
+		{"100e1", "1e3"},
+		{"1.1e+1", "11"},
+		{"1.1e6", "11e5"},
+		{"0.252", ".252"},
+		{"1.252", "1.252"},
+		{"-1.252", "-1.252"},
+		{"0.075", ".075"},
+		{"789012345678901234567890123456789e9234567890123456789", "789012345678901234567890123456789e9234567890123456789"},
+		{".000100009", "100009e-9"},
+		{".0001000009", ".0001000009"},
+		{".0001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000009", ".0001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000009"},
+		{"E\x1f", "E\x1f"}, // fuzz
+		//{"96px", "1in"},
+
+		{".12345e8", "12345e3"},
+		{".12345e7", "1234500"},
+		{".12345e6", "123450"},
+		{".12345e5", "12345"},
+		{".012345e6", "12345"},
+		{".12345e4", "1234.5"},
+		{"-.12345e4", "-1234.5"},
+		{".12345e0", ".12345"},
+		{".12345e-1", ".012345"},
+		{".12345e-2", ".0012345"},
+		{".12345e-3", "12345e-8"},
+		{".12345e-4", "12345e-9"},
+		{".12345e-5", "12345e-10"},
+
+		{".123456e-3", "123456e-9"},
+		{".123456e-2", ".00123456"},
+		{".1234567e-4", "1234567e-11"},
+		{".1234567e-3", ".0001234567"},
+
+		{"12345678e-1", "1234567.8"},
+		{"72.e-3", ".072"},
+		{"7640e-2", "76.4"},
+		{"10.e-3", ".01"},
+		{".0319e3", "31.9"},
+	}
+	for _, tt := range numberTests {
+		//fmt.Println("\n"+tt.number, "->", tt.expected)
+		test.Minify(t, tt.number, nil, string(Number([]byte(tt.number), -1)), tt.expected)
+	}
+}
+
+func TestNumberTruncate(t *testing.T) {
+	numberTests := []struct {
+		number   string
 		truncate int
 		expected string
 	}{
-		{"0", -1, "0"},
-		{".0", -1, "0"},
-		{"1.0", -1, "1"},
-		{"0.1", -1, ".1"},
-		{"+1", -1, "1"},
-		{"-1", -1, "-1"},
-		{"-0.1", -1, "-.1"},
-		{"100", -1, "100"},
-		{"1000", -1, "1e3"},
-		{"0.001", -1, ".001"},
-		{"0.0001", -1, "1e-4"},
-		{"100e1", -1, "1e3"},
-		{"1.1e+1", -1, "11"},
-		{"0.252", -1, ".252"},
-		{"1.252", -1, "1.252"},
-		{"-1.252", -1, "-1.252"},
-		{"0.075", -1, ".075"},
-		{"789012345678901234567890123456789e9234567890123456789", -1, "789012345678901234567890123456789e9234567890123456789"},
-		{".000100009", -1, "100009e-9"},
-		{".0001000009", -1, ".0001000009"},
-		{".0001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000009", -1, ".0001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000009"},
-		{"E\x1f", -1, "E\x1f"}, // fuzz
-		//{"96px", "1in"},
-
-		// truncate
 		{"0.1", 1, ".1"},
 		{"0.075", 1, ".1"},
 		{"0.025", 1, "0"},
 		{"0.0001", 1, "1e-4"},
 	}
 	for _, tt := range numberTests {
-		test.Minify(t, tt.number, nil, string(Number([]byte(tt.number), tt.truncate)), tt.expected)
+		test.Minify(t, tt.number, nil, string(Number([]byte(tt.number), tt.truncate)), tt.expected, "truncate to", tt.truncate)
+	}
+}
+
+func TestNumberRandom(t *testing.T) {
+	N := int(1e6)
+	if testing.Short() {
+		N = 0
+	}
+	for i := 0; i < N; i++ {
+		b := RandNumBytes()
+		f, _ := strconv.ParseFloat(string(b), 64)
+
+		b2 := make([]byte, len(b))
+		copy(b2, b)
+		b2 = Number(b2, -1)
+		f2, _ := strconv.ParseFloat(string(b2), 64)
+		if math.Abs(f-f2) > 1e-6 {
+			fmt.Println("Bad:", f, "!=", f2, "in", string(b), "to", string(b2))
+		}
 	}
 }
 
 ////////////////
+
+var n = 100
+var numbers [][]byte
+
+func TestMain(t *testing.T) {
+	numbers = make([][]byte, 0, n)
+	for j := 0; j < n; j++ {
+		numbers = append(numbers, RandNumBytes())
+	}
+}
 
 func RandNumBytes() []byte {
 	var b []byte
@@ -114,7 +182,7 @@ func RandNumBytes() []byte {
 		if rand.Int()%2 == 0 {
 			b = append(b, '-')
 		}
-		n = rand.Int() % 5
+		n = 1 + rand.Int()%4
 		for i := 0; i < n; i++ {
 			b = append(b, byte(rand.Int()%10)+'0')
 		}
@@ -124,6 +192,15 @@ func RandNumBytes() []byte {
 
 func BenchmarkNumber(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Number(RandNumBytes(), -1)
+		for j := 0; j < n; j++ {
+			Number(numbers[j], -1)
+		}
+	}
+}
+
+func BenchmarkNumber2(b *testing.B) {
+	num := []byte("1.2345e-6")
+	for i := 0; i < b.N; i++ {
+		Number(num, -1)
 	}
 }
