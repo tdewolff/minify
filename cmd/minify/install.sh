25facb3d173b13311e7fa6f596abca0fbb55ba9a
Taco de Wolff <tacodewolff@gmail.com>
2020-01-23 17:32:11 -0300

    install.sh: check if git exists and we're in a git repository

--- cmd/minify/install.sh
+++ cmd/minify/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-go install -ldflags "-s -w -X 'main.Version=$(git describe --tags)'" -trimpath
+if command -v git >/dev/null 2>&1 && git rev-parse --git-dir >/dev/null 2>&1; then
+    go install -ldflags "-s -w -X 'main.Version=$(git describe --tags)'" -trimpath
+else
+    go install -ldflags "-s -w" -trimpath
+fi
 
 source minify_bash_tab_completion
