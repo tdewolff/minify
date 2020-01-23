2f2c0aedbaa90c8bdca2e940ea35d5b9efc77983
Taco de Wolff <tacodewolff@gmail.com>
2020-01-23 13:49:08 -0300

    Add trimpath to install.sh

--- cmd/minify/install.sh
+++ cmd/minify/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-go install -ldflags "-s -w -X 'main.Version=$(git describe --tags)'"
+go install -ldflags "-s -w -X 'main.Version=$(git describe --tags)'" -trimpath
 
 source minify_bash_tab_completion
