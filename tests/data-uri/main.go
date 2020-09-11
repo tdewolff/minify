1bd30d887a9f05f203ffecf8d6c9d087a17b42e8
Taco de Wolff <tacodewolff@gmail.com>
2020-09-10 22:44:29 -0300

    Update data-uri test

--- tests/data-uri/main.go
+++ tests/data-uri/main.go
@@ -1,10 +1,13 @@
 // +build gofuzz
 package fuzz
 
-import "github.com/tdewolff/minify/v2"
+import (
+	"github.com/tdewolff/minify/v2"
+	"github.com/tdewolff/parse/v2"
+)
 
 func Fuzz(data []byte) int {
-	m := minify.New()
-	data = minify.DataURI(m, data)
+	data = parse.Copy(data) // ignore const-input error for OSS-Fuzz
+	data = minify.DataURI(minify.New(), data)
 	return 1
 }
