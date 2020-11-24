618eef4cc0ce5db3f870f9a1de8b018c66178fff
Taco de Wolff <tacodewolff@gmail.com>
2020-11-24 09:39:29 +0100

    Update oss-fuzz build script to use compile_go_fuzzer

--- tests/oss-fuzz-build.sh
+++ tests/oss-fuzz-build.sh
@@ -1,22 +1,13 @@
-function compile_fuzzer {
-  path=$1
-  function=$2
-  fuzzer=$3
-
-  go-fuzz -func $function -o $fuzzer.a $path
-
-  $CXX $CXXFLAGS $LIB_FUZZING_ENGINE $fuzzer.a -o $OUT/$fuzzer
-}
-
+# compile_go_fuzzer can be found in the oss-fuzz repository
 git clone https://github.com/tdewolff/parse
 find $GOPATH/src/github.com/tdewolff/parse/tests/* -maxdepth 0 -type d | while read target
 do
     fuzz_target=`echo $target | rev | cut -d'/' -f 1 | rev`
-    compile_fuzzer github.com/tdewolff/parse/tests/$fuzz_target Fuzz parse-$fuzz_target-fuzzer
+    compile_go_fuzzer github.com/tdewolff/parse/tests/$fuzz_target Fuzz parse-$fuzz_target-fuzzer
 done
 
 find $GOPATH/src/github.com/tdewolff/minify/tests/* -maxdepth 0 -type d | while read target
 do
     fuzz_target=`echo $target | rev | cut -d'/' -f 1 | rev`
-    compile_fuzzer github.com/tdewolff/minify/tests/$fuzz_target Fuzz minify-$fuzz_target-fuzzer
+    compile_go_fuzzer github.com/tdewolff/minify/tests/$fuzz_target Fuzz minify-$fuzz_target-fuzzer
 done
