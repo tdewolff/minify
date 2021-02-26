43e6042885eb8c709f0eeb236c24ba861d1705c7
Taco de Wolff <tacodewolff@gmail.com>
2021-02-26 10:36:13 +0100

    Fix OSS-Fuzz build script

--- tests/oss-fuzz-build.sh
+++ tests/oss-fuzz-build.sh
@@ -1,14 +1,15 @@
 # compile_go_fuzzer can be found in the oss-fuzz repository
-cd parse
-find tests/* -maxdepth 0 -type d | while read target
+root=$(pwd)
+find parse/tests/* -maxdepth 0 -type d | while read target
 do
+    cd $root/$d
     fuzz_target=`echo $target | rev | cut -d'/' -f 1 | rev`
     compile_go_fuzzer github.com/tdewolff/parse/tests/$fuzz_target Fuzz parse-$fuzz_target-fuzzer
 done
 
-cd ../minify
-find tests/* -maxdepth 0 -type d | while read target
+find minify/tests/* -maxdepth 0 -type d | while read target
 do
+    cd $root/$d
     fuzz_target=`echo $target | rev | cut -d'/' -f 1 | rev`
     compile_go_fuzzer github.com/tdewolff/minify/tests/$fuzz_target Fuzz minify-$fuzz_target-fuzzer
 done
