96b8e89a6130b8706a9b5b6d7c1fb0ff6754094d
Taco de Wolff <tacodewolff@gmail.com>
2022-06-23 15:44:44 -0400

    Incomplete work on the Node-API binding, missing config function, see #379

--- bindings/js/binding.gyp
+++ bindings/js/binding.gyp
@@ -2,10 +2,9 @@
     "targets": [{
         "target_name": "minify",
         "product_extension": "node",
-        "include_dirs": ["<!(node -e \"require('nan')\")"],
         "type": "<(library)",
         "cflags": ["-Wall"],
-        "sources": ["minify.cc"],
+        "sources": ["minify.c"],
         "libraries": ["../minify.a"],
     }],
 }
--- bindings/js/test.js
+++ bindings/js/test.js
@@ -1,5 +1,7 @@
-import { config, string } from '@tdewolff/minify';
+import { config, string, file } from '@tdewolff/minify';
 
-config({'html-keep-document-tags': 5.0})
+config({'html-keep-document-tags': "a"})
 
 console.log(string("text/html", "<html><span style=\"color:#ff0000;\">A  phrase</span>"));
+
+file("text/html", "test.html", "test.min.html");
