a01c7d65bfb34546553bb6cb78190ca318d6cb80
Julian Grinblat <julian@dotcore.co.il>
2022-07-03 22:42:53 +0900

    Reproduce segmantation fault

--- bindings/js/test/index.js
+++ bindings/js/test/index.js
@@ -1,5 +1,4 @@
 import { config, string } from '@tdewolff/minify';
-import { Worker } from 'node:worker_threads';
 
 config({'html-keep-document-tags': true})
 
@@ -9,19 +8,3 @@ let output = string("text/html", input);
 if (output != expected) {
     throw "unexpected output: '"+output+"' instead of '"+expected+"'";
 }
-
-const worker = new Worker(new URL("worker.js", import.meta.url), {
-    workerData: input,
-});
-output = await new Promise((resolve, reject) => {
-    worker.on('message', resolve);
-    worker.on('error', reject);
-    worker.on('exit', (code) => {
-        if (code !== 0)
-        reject(new Error(`Worker stopped with exit code ${code}`));
-    });
-})
-if (output != expected) {
-    throw "unexpected output using worker threads: '"+output+"' instead of '"+expected+"'";
-}
-await worker.terminate();
--- bindings/js/test/worker.js
+++ bindings/js/test/worker.js
@@ -1,5 +1,26 @@
-import { config, string } from '@tdewolff/minify';
-import { workerData, parentPort } from 'node:worker_threads';
+import { Worker, isMainThread, workerData, parentPort } from 'node:worker_threads';
 
-config({'html-keep-document-tags': true})
-parentPort.postMessage(string("text/html", workerData));
+if (isMainThread) {
+    let input = "<html><span style=\"color:#ff0000;\">A  phrase</span>";
+    const worker = new Worker(new URL(import.meta.url), {
+        workerData: input,
+    });
+
+    let expected = "<html><span style=color:red>A phrase</span>";
+    let output = await new Promise((resolve, reject) => {
+        worker.on('message', resolve);
+        worker.on('error', reject);
+        worker.on('exit', (code) => {
+            if (code !== 0)
+            reject(new Error(`Worker stopped with exit code ${code}`));
+        });
+    })
+    if (output != expected) {
+        throw "unexpected output using worker threads: '"+output+"' instead of '"+expected+"'";
+    }
+    await worker.terminate();
+} else {
+    const { config, string } = await import('@tdewolff/minify');
+    config({'html-keep-document-tags': true})
+    parentPort.postMessage(string("text/html", workerData));
+}
