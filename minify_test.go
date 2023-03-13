d6c4fa72b321546f11bef60bd5a26de8be2e4367
Taco de Wolff <tacodewolff@gmail.com>
2023-03-13 17:18:27 -0300

    Check failing test

--- minify_test.go
+++ minify_test.go
@@ -128,6 +128,7 @@ func TestAdd(t *testing.T) {
 	w.Reset()
 
 	s := mAdd.Minify("dummy/err", w, r).Error()
+	fmt.Println(s)
 	test.String(t, s[len(s)-5:], "error")
 
 	s = mAdd.Minify("werr6", w, r).Error()
