3419d9451fd492e0462e6cdd05bf7f93445032ae
Daniel Foerster <pydsigner@gmail.com>
2023-10-17 20:20:33 -0500

    Try to fix missing .so on windows Python builds (2)

--- bindings/py/setup.py
+++ bindings/py/setup.py
@@ -60,8 +60,6 @@ setup(
     cmdclass={"build_ext": build_ext_external},
     packages=["minify"],
     package_dir={"": "src"},
-    package_data={"minify": ["_minify.so"]},
-    include_package_data=True,
     setup_requires=["cffi>=1.0.0"],
     cffi_modules=["build_minify.py:ffi"],
     install_requires=["cffi>=1.0.0"],
