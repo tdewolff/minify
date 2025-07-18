c40b7c4564b907c3ec9f9e8cd111fc7608ef0df4
Taco de Wolff <tacodewolff@gmail.com>
2025-07-18 11:10:09 +0200

    Update Python version

--- bindings/py/setup.py
+++ bindings/py/setup.py
@@ -42,6 +42,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Software Development :: Pre-processors",
         "Topic :: Text Processing :: Markup",
